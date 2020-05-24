8837aa866c1be7e59ff4dcf62c32cf8c1e3b1726
Mayank-rathi <Mayankrathi33@gmail.com>
2020-05-24 11:24:23 +0530

    ADDED:Code To Generate Random Memes

---
+++ src/component/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>MemeGenerator</h2>
+            </div>
+        )
+    }
+}
+
+export default MainPage
